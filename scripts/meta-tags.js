b15ba8049898bc93c77e31a5cc888518dce38212
I am <88476416+PrimeSolar@users.noreply.github.com>
2026-09-07 00:01:56 +0300

    Improve the web page identification, navigation, branding, and SEO
    * Updated the <title> tag content to improve the web page identification, navigation, branding, and search engine optimization (SEO).
    * Upgraded the jQuery dependency from v.3.7.1 to v.4.0.0.
    * Created and updated commentaries using JSDoc format to clarify the code logic.

--- scripts/meta-tags.js
+++ scripts/meta-tags.js
@@ -1,5 +1,23 @@
 /*
- * Script
+ * Configuration Script
+ *
+ * This script sets up the important HTML structure for the web application.
+ * It dynamically inserts metadata and styles to configure the document's head elements
+ * for optimal SEO, accessibility, and user experience.
+ *
+ * Key Features:
+ * - Sets the default language of the document to English (`lang = "en"`).
+ * - Defines the default text direction as left-to-right (`dir = "ltr"`).
+ * - Configures the document title for the web page.
+ * - Links to the style sheet for styling the application.
+ * - Adds a badge to enhance brand identity.
+ * - Specifies the type attribute for all script elements for better compatibility.
+ *
+ * Any corresponding manual changes in the HTML document would be overridden at runtime.
+ * The <meta> charset tag is hardcoded because it needs to be guaranteed to be within the first 1024 bytes of the HTML document,
+ * as some browsers only look at those bytes before choosing an encoding
+ * (see https://html.spec.whatwg.org/multipage/semantics.html#charset).
+ * The <meta> viewport tag is hardcoded to ensure the immediate correct page rendering.
  *
  * Copyright © Vladislav Kazantsev
  * All rights reserved.
@@ -13,31 +31,29 @@
  * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
  */
 
-// Set the <html> element `lang` property
+/** Set the <html> element `lang` property. */
 const html = document.querySelector("html");
 html.lang = "en";
 
-// Set `dir="ltr"` for the <html> element
+/**
+ * The script includes detailed comments
+ * to support stakeholders with varying JS knowledge.
+ */
+/** Set `dir="ltr"` for the <html> element. */
 html.dir = "ltr";
 
-// Set <meta name="viewport" content="width=device-width, initial-scale=1" />
-const metaViewport = document.createElement("meta");
-metaViewport.name = "viewport";
-metaViewport.content = "width=device-width, initial-scale=1";
-document.querySelector("head").appendChild(metaViewport);
-
-// Set `title` for the <head> element
+/** Set `title` for the <head> element. */
 const title = document.createElement("title");
-title.textContent = "Simon Game";
+title.textContent = "Simon Game | Full Stack Developer Vladislav Kazantsev";
 document.querySelector("head").appendChild(title);
 
-// Insert <link rel="canonical" href="https://primesolar.github.io/simon-game/" />
+/** Insert <link rel="canonical" href="https://primesolar.github.io/simon-game/" />. */
 const linkCanonical = document.createElement("link");
 linkCanonical.rel = "canonical";
 linkCanonical.href = "https://primesolar.github.io/simon-game/";
 document.querySelector("head").appendChild(linkCanonical);
 
-// Insert <link rel="stylesheet" type="text/css" href="styles/style-min.css">
+/** Insert <link rel="stylesheet" type="text/css" href="styles/style-min.css">. */
 const linkStyle = document.createElement("link");
 linkStyle.rel = "stylesheet";
 linkStyle.type = "text/css";
@@ -48,19 +64,19 @@ linkStyle.onload = function () {
   document.body.style.flexDirection = "column";
 };
 
-// Insert <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Pixelify+Sans">
+/** Insert <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Pixelify+Sans">. */
 const linkFont = document.createElement("link");
 linkFont.rel = "stylesheet";
 linkFont.type = "text/css";
 linkFont.href = "https://fonts.googleapis.com/css?family=Pixelify+Sans";
 document.querySelector("head").appendChild(linkFont);
 
-// Insert <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+/** Insert <script src="https://code.jquery.com/jquery-4.0.0.min.js"></script>. */
 const jQuery = document.createElement("script");
-jQuery.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
+jQuery.src = "https://code.jquery.com/jquery-4.0.0.min.js";
 document.querySelector("body").appendChild(jQuery);
 
-// Insert <script src="scripts/game-min.js"></script> and <script src="scripts/components-min.js"></script>
+/** Insert <script src="scripts/game-min.js"></script> and <script src="scripts/components-min.js"></script>. */
 jQuery.onload = function () {
   const gameScript = document.createElement("script");
   gameScript.src = "scripts/game-min.js";
@@ -70,13 +86,13 @@ jQuery.onload = function () {
   document.querySelector("body").appendChild(script);
 };
 
-// Insert <link rel="icon" href="pics/logos/logo.jpg">
+/** Insert <link rel="icon" href="pics/logos/logo.jpg">. */
 const linkImage = document.createElement("link");
 linkImage.rel = "icon";
 linkImage.href = "assets/images/logo.jpg";
 document.querySelector("head").appendChild(linkImage);
 
-// Set `type="text/javascript"` for all <script> elements for better compatibility
+/** Set `type="text/javascript"` for all <script> elements for better compatibility. */
 for (let x of document.querySelectorAll("script")) {
   x.type = "text/javascript";
 }
